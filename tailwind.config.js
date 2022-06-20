76900edd294618c616efefdd1e6d617b51d1e5e2
Kacper Malek <kacper@growl.be>
2022-06-20 21:18:46 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      serif: ["Montserrat", "serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
